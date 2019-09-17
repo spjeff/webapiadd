aad417288d147d98e91b9841e97b5bf8c0280ed6
Jeff Jones <spjeff@spjeff.com>
2019-09-17 13:10:40 -0500

    Init
    * Register new Enterprise App
     * Tenant and App GUID
     * Download sample ZIP
     * Restore NuGet packages
     * Install CORS package
     * Verify with Chrome and Fiddler

---
+++ AppModelv2-WebApp-OpenIDConnect-DotNet/App_Start/WebApiConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace AppModelv2_WebApp_OpenIDConnect_DotNet
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            //Enable CORS
+            config.EnableCors();
+
+            //REM config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ AppModelv2-WebApp-OpenIDConnect-DotNet/Controllers/HelloController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace AppModelv2_WebApp_OpenIDConnect_DotNet.Controllers
+{
+    [EnableCors(origins: "*",
+    headers: "*",
+    methods: "*",
+    SupportsCredentials = true)]
+    [Authorize]
+    public class HelloController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            DateTime now = DateTime.Now;
+            return new string[] { "Hello", now.ToLongDateString() + " " + now.ToLongTimeString() };
+        }
+    }
+}
